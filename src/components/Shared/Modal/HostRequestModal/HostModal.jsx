66df94a8e5994b3475deb70ad4cb92b03af4c678
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-01-30 14:47:07 +0600

    host request modal added and updated

--- src/components/Shared/Modal/HostRequestModal/HostModal.jsx
+++ src/components/Shared/Modal/HostRequestModal/HostModal.jsx
@@ -13,7 +13,7 @@ const HostModal = ({ modalHandler, closeModal, isOpen }) => {
           leaveFrom='opacity-100'
           leaveTo='opacity-0'
         >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
+          <div className='fixed inset-0 bg-black/40 bg-opacity-25' />
         </Transition.Child>
 
         <div className='fixed inset-0 overflow-y-auto'>
--- src/components/Shared/Navbar/PublicNavbar.jsx
+++ src/components/Shared/Navbar/PublicNavbar.jsx
@@ -1,5 +1,5 @@
-'use client'
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import logo from "../../../assets/logo/logo.png";
 import darklogo from "../../../assets/logo/dark-logo.png";
 import Link from "next/link";
@@ -8,9 +8,11 @@ import Image from "next/image";
 import ModeToggle from "../ModeToggle/ModeToggle";
 import MobileMenu from "../MobileMenu/MobileMenu";
 import UserInfo from "@/components/Auth/NavBarUserInfo/UserInfo";
-
+import HostModal from "../Modal/HostRequestModal/HostModal";
+import { useAuth } from "@/Hooks/useAuth";
 
 const PublicNavbar = () => {
+  const {userInfo}=useAuth()
   const navItems = [
     { href: "/home", label: "Home" },
     { href: "/all-rooms", label: "All Rooms" },
@@ -19,9 +21,17 @@ const PublicNavbar = () => {
     { href: "/host/dashboard", label: "Dashboard" },
     { href: "/contact", label: "Contact Us" },
   ];
- 
-  
 
+  // host req modal
+  const [isOpen, setIsOpen] = useState(false);
+
+
+
+  const modalHandler=async()=>{
+console.log('i want to be a host accpet requ')
+
+setIsOpen(false)
+  }
   return (
     <header className="bg-background dark:bg-background">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
@@ -56,9 +66,24 @@ const PublicNavbar = () => {
           ))}
         </nav>
         <div className="auth-btns hidden md:flex items-center space-x-5">
+          {/* host apply button div container */}
+        {
+          userInfo &&   <div>
+            <button
+            onClick={() => setIsOpen(true)}
+            className="cursor-pointer text-xl font-bold text-slate-600 animate-skeletonLoader"
+          >
+            Host Your Home
+          </button>
+          {/* host req modal here below */}
+
+          <HostModal isOpen={isOpen} closeModal={() => setIsOpen(false)} modalHandler={modalHandler}/>
+          </div>
+        }
+
           <ModeToggle />
-         
-         <UserInfo/>
+
+          <UserInfo />
 
           {/* Mobile Menu */}
         </div>
