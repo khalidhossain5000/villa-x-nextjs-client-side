9258b83e690f624eadb6d2d1cdca192e305cea19
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-09 15:09:19 +0600

    private route added to more page.js

--- src/app/(dashboardLayout)/admin/dashboard/page.js
+++ src/app/(dashboardLayout)/admin/dashboard/page.js
@@ -1,12 +1,15 @@
+import PrivateRoute from '@/components/PrivateRoute/PrivateRoute';
 import React from 'react';
 
 const AdminDashboardHomePage  = () => {
     return (
+                 <PrivateRoute requiredRoles={['admin']}>
         <div>
             <h2>Welcome to Admin dashboard page ove r here</h2>
            
       
         </div>
+        </PrivateRoute>
     );
 };
 
--- src/app/(dashboardLayout)/host/dashboard/page.js
+++ src/app/(dashboardLayout)/host/dashboard/page.js
@@ -1,10 +1,13 @@
+import PrivateRoute from '@/components/PrivateRoute/PrivateRoute';
 import React from 'react';
 
 const HostDashboardHomePage  = () => {
     return (
+         <PrivateRoute requiredRoles={['host']}>
         <div>
             <h2>Welcome to host dashboard page ove r here</h2>
         </div>
+        </PrivateRoute>
     );
 };
 
