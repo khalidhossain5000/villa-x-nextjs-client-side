b2683c1dbaedd8f2156d78610b7169cce338d88e
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-09 22:00:10 +0600

    added

--- src/app/(dashboardLayout)/guest/dashboard/layout.js
+++ src/app/(dashboardLayout)/guest/dashboard/layout.js
@@ -1,6 +1,6 @@
 import PrivateRoute from "@/components/PrivateRoute/PrivateRoute";
 import React from "react";
-
+//guest dashboard this is over here layout
 const GuestDashboardLayout = ({ children }) => {
   return <PrivateRoute requiredRoles={["guest"]}>
     <div>{children}</div>
