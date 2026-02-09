f3fb6d5012fdb7284d8cb012d90d262cb859b0ea
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-09 09:52:58 +0600

    updated

--- src/app/(commonLayout)/all-rooms/page.js
+++ src/app/(commonLayout)/all-rooms/page.js
@@ -1,24 +1,23 @@
-import Rooms from '@/components/HomePage/RoomsSection/Rooms';
-import Categories from '@/components/Shared/HomePage/Categories/Categories';
-import React from 'react';
+import Rooms from "@/components/HomePage/RoomsSection/Rooms";
+import Categories from "@/components/Shared/HomePage/Categories/Categories";
+import React from "react";
 
 const AllRooms = () => {
-    return (
-        <div className="px-20 pt-20">
-            <h2>This is all rooms page start over here</h2>
+  return (
+    <div className="px-20 pt-20">
+      <h2>This is all rooms page start over here</h2>
 
-            {/* categories */}
+      {/* categories */}
 
-
-            <div>
-                <Categories></Categories>
-            </div>
-            {/* allrooms data is here */}
-            <div>
-                <Rooms/>
-            </div>
-        </div>
-    );
+      <div>
+        <Categories></Categories>
+      </div>
+      {/* allrooms data is here */}
+      <div>
+        <Rooms />
+      </div>
+    </div>
+  );
 };
 
 export default AllRooms;
