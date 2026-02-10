762c4c1da855e1368517d2f072b203b225c51201
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-10 21:50:50 +0600

    homep age is update dnotv

--- src/app/(commonLayout)/page.js
+++ src/app/(commonLayout)/page.js
@@ -1,7 +1,6 @@
 import Banner from "@/components/HomePage/Banner/Banner";
 import Rooms from "@/components/HomePage/RoomsSection/Rooms";
-import Image from "next/image";
-import logo from '../../assets/logo/logohdsg.png'
+
 import React, { Suspense } from "react";
 
 const HomePage = () => {
@@ -12,23 +11,12 @@ const HomePage = () => {
         <Rooms />
       </Suspense>
 
-
-      <h2 className="text-4xl text-black py-12 font-bold font-playfair">THis is play fair disply & camping & climbing  
-</h2>
-      <h2 className="text-4xl text-green-400 py-12 font-bold font-raleway">THis is raleway fotn font over here fair disply &&&&&</h2>
-
-
-
-
-
-
-       <Image
-                    src={logo}
-                    alt="Logo"
-                    width={150}
-                    height={50}
-                    className="bg- "
-                  />
+      <h2 className="text-4xl text-black py-12 font-bold font-playfair">
+        THis is play fair disply & camping & climbing
+      </h2>
+      <h2 className="text-4xl text-green-400 py-12 font-bold font-raleway">
+        THis is raleway fotn font over here fair disply &&&&&
+      </h2>
     </div>
   );
 };
