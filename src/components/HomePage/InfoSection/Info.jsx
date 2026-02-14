c21654d92369449239034dc37a09b19454f2e2aa
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-14 22:02:56 +0600

    info added

--- src/components/HomePage/InfoSection/Info.jsx
+++ src/components/HomePage/InfoSection/Info.jsx
@@ -1,64 +1,71 @@
-import HomeTitle from '@/components/Shared/HomePage/HomeTitle/HomeTitle';
-import React from 'react';
-import hiking from '../../../assets/Home/Info/Main-custom-icon-1.png'
-import climbing from '../../../assets/Home/Info/Main-custom-icon-2.png'
-import walking from '../../../assets/Home/Info/Main-custom-icon-3.png'
-import cycling from '../../../assets/Home/Info/Main-custom-icon-4.png'
-import canoeing from '../../../assets/Home/Info/Main-custom-icon-5.png'
-import orienteering from '../../../assets/Home/Info/Main-custom-icon-6.png'
+import HomeTitle from "@/components/Shared/HomePage/HomeTitle/HomeTitle";
+import React from "react";
+import hiking from "../../../assets/Home/Info/Main-custom-icon-1.png";
+import climbing from "../../../assets/Home/Info/Main-custom-icon-2.png";
+import walking from "../../../assets/Home/Info/Main-custom-icon-3.png";
+import cycling from "../../../assets/Home/Info/Main-custom-icon-4.png";
+import canoeing from "../../../assets/Home/Info/Main-custom-icon-5.png";
+import orienteering from "../../../assets/Home/Info/Main-custom-icon-6.png";
+import Image from "next/image";
 const Info = () => {
-    const activities = [
-  {
-    title: "Hiking",
-    icon:hiking,
-    subtitle: "Explore rugged trails and embrace nature's calm winds"
-  },
-  {
-    title: "Climbing",
-    icon:climbing,
-    subtitle: "Scale rocky heights and conquer thrilling vertical challenges"
-  },
-  {
-    title: "Walking",
-    icon:walking,
-    subtitle: "Stroll scenic paths and enjoy peaceful outdoor moments"
-  },
-  {
-    title: "Cycling",
-    icon:cycling,
-    subtitle: "Ride winding routes and feel the rush of adventure"
-  },
-  {
-    title: "Canoeing",
-    icon:canoeing,
-    subtitle: "Paddle quiet rivers and explore serene watersides"
-  },
-  {
-    title: "Orienteering",
-    icon:orienteering,
-    subtitle: "Navigate wild terrain using maps and sharp instincts"
-  }
-];
-
-    return (
-        <div>
-            <HomeTitle
-            title=""
-            />
-
-<div>
+  const activities = [
     {
-        activities.map((act,i)=><div
-        key={i}
-        >
-            
-        </div>)
-    }
-</div>
+      title: "Hiking",
+      icon: hiking,
+      subtitle: "Explore rugged trails and embrace nature's calm winds",
+    },
+    {
+      title: "Climbing",
+      icon: climbing,
+      subtitle: "Scale rocky heights and conquer thrilling vertical challenges",
+    },
+    {
+      title: "Walking",
+      icon: walking,
+      subtitle: "Stroll scenic paths and enjoy peaceful outdoor moments",
+    },
+    {
+      title: "Cycling",
+      icon: cycling,
+      subtitle: "Ride winding routes and feel the rush of adventure",
+    },
+    {
+      title: "Canoeing",
+      icon: canoeing,
+      subtitle: "Paddle quiet rivers and explore serene watersides",
+    },
+    {
+      title: "Orienteering",
+      icon: orienteering,
+      subtitle: "Navigate wild terrain using maps and sharp instincts",
+    },
+  ];
 
+  return (
+    <div className='bg-primary py-14 lg:py-24'>
+        
+      <HomeTitle title="" />
 
-        </div>
-    );
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center gap-x-9 gap-y-16 px-2 xl:px-0">
+        {activities.map((act, i) => (
+          <div key={i} className="flex items-center gap-6 gap-y-16">
+            <div className="img">
+                <Image
+                src={act.icon}
+                alt='acttivites icon'
+                width={150}
+                height={50}
+                />
+            </div>
+            <div className="content">
+              <h2 className="text-slate-100 text-xl font-medium pb-4">{act.title}</h2>
+              <h5 className="text-slate-100 ">{act.subtitle}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Info;
--- src/components/HomePage/RoomSlider/RoomSlider.jsx
+++ src/components/HomePage/RoomSlider/RoomSlider.jsx
@@ -63,7 +63,20 @@ const RoomSlider = () => {
             disableOnInteraction: false,
           }}
           loop={true}
-          //   grabCursor={true}
+          breakpoints={{
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      }}
         >
           {allRoomData.map((room) => (
             
