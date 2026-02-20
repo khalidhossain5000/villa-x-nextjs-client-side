6ae771b676cca83828c89128cd665186066dcb1d
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-20 21:43:50 +0600

    unnecs code is remove hre

--- src/components/AllRoomsPage/SingleRoomPage/Calendar.jsx
+++ src/components/AllRoomsPage/SingleRoomPage/Calendar.jsx
@@ -1,50 +1,3 @@
-// /* eslint-disable react/prop-types */
-// import { DateRange } from "react-date-range";
-
-// const Calender = ({ value, handleDateChange, room, disabledRanges }) => {
-//   const isDateBlocked = (date) => {
-//     return disabledRanges?.some(
-//       (range) => date >= range.startDate && date <= range.endDate,
-//     );
-//   };
-//   return (
-
-//     <DateRange
-//       ranges={[value]}
-//       rangeColors={["#0fe60e"]}
-//       direction="vertical"
-//       showDateDisplay={false}
-//       onChange={handleDateChange}
-//       minDate={new Date(room?.from)}
-//       maxDate={new Date(room?.to)}
-//       disabledDay={isDateBlocked}
-
-//         wrapperClassName="bg-[#00002c] dark:bg-gray-900 p-2 lg:p-6 rounded-lg dark:text-white" // outer wrapper
-
-//       dayContentRenderer={(date) => {
-//         const blocked = isDateBlocked(date);
-//         return (
-//           <div
-//             className={`  w-full h-full flex items-center justify-center rounded-full
-//         ${blocked ? "bg-red-500 text-green-600" : "text-black "}`}
-//           >
-//             {date.getDate()}
-//           </div>
-//         );
-//       }}
-//     />
-
-//   );
-// };
-
-// export default Calender;
-
-
-
-
-
-
-
 
 'use client';
 
@@ -130,8 +83,6 @@ const Calender = ({ value, handleDateChange, room, disabledRanges }) => {
 };
 
 export default Calender;
-
-
 
 
 
