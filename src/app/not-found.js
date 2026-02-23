89c9e3244b58c1a009e1bb7498a51360bd376a56
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-23 16:53:09 +0600

    not found page is added

---
+++ src/app/not-found.js
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-6xl font-bold text-red-500">404</h1>
+      <p className="text-xl mt-4">Page Not Found</p>
+      <button
+       
+        className="mt-6 px-6 py-3 bg-red-500 hover:bg-red-600 rounded-lg"
+      >
+        Go Back Home
+      </button>
+    </div>
+  );
+}
