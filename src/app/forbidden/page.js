3cf2a45a1545647cb7cf3a372480bcbf8ec58fb3
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-25 12:41:15 +0600

    forbidden page is updated now

--- src/app/forbidden/page.js
+++ src/app/forbidden/page.js
@@ -1,21 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import imgi from "../../../assets/forbiddenImg/forbidden-img.jpg";
+import imgi from "../../assets/forbiddenImg/nforbidden.png";
 import Link from "next/link";
 
 
 const Forbidden = () => {
   return (
     <div className="container lg:max-w-full lg:px-12 mx-auto min-h-screen shadow-2xl ">
-      <h1 className="text-3xl lg:text-7xl text-center text-[#4a3993] font-bold rancho py-6 lg:py-9">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl 2xl:text-6xl font-raleway font-bold text-[#f9a300] text-center   py-6 lg:py-9">
         Oops! You Shall Not Pass!
       </h1>
-      <h1 className="text-xl py-3 lg:py-0 lg:text-xl font-bold text-center">
+      <h1 className="text-xl lg:text-2xl py-3 lg:py-6 lg:text-xl font-bold text-center">
         Looks like this page is more secret than your browser history. Try going
         back before Gandalf gets mad.
       </h1>
       <div className="imgs text-center ">
-        <img src={imgi} className="text-center mx-auto lg:w-7/12" alt="" />
+        <img src={imgi.src} className="text-center mx-auto lg:w-7/12" alt="" />
       </div>
       <div className="w-full  text-center py-4">
         <Link
